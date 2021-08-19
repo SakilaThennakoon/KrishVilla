11700e6f19489458f41418ac23015ceb8e32d8e1
SakilaThennakoon <sakila.heshan@gmail.com>
2021-08-19 21:06:07 +0530

    s

--- application/views/admin/ImageUpload.php
+++ application/views/admin/ImageUpload.php
@@ -1,22 +1,7 @@
 <div class="container" style="padding-top: 20px;">
-    <h3  style="text-align: center; color: #82ae46;">Cust Register</h3>  
+    <h3  style="text-align: center; color: #82ae46;">Image Upload</h3>  
 
 
-   
-        <div class="col-md-4">
-          <label for="inputeMail" class="form-label">Email</label>
-          <input id="email" name="email" type="email" class="form-control">
-          <input id="hidden" name="hidden" type="hidden" value="customer">
-        </div>
-        <div class="col-md-5">
-          <label for="inputpassWord" class="form-label">Password</label>
-          <input id="passWord" name="passWord" type="password" class="form-control">
-        </div>
-
-        <div class="col-md-10">
- 
-          </div>
-          <div>
          
           <?php echo form_open_multipart('admin/imageUpload');?>
 
